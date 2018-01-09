8f282f20a0dd34c61ad0aa3528d81592d8170f5d
unknown <monkpuah123@gmail.com>
2018-01-10 00:38:55 +0800

    display text change accordingly base number or text

--- js/functions.js
+++ js/functions.js
@@ -5,9 +5,9 @@ document.onkeydown = function(e) {
 	let extension = '.mp3';
 
 	if (mapper[e.keyCode]) {
-		let alphabet = mapper[e.keyCode];
-		audioContainer.setAttribute('src', basePath + alphabet + extension);
-		displayAlphabet(alphabet);
+		let char = mapper[e.keyCode];
+		audioContainer.setAttribute('src', basePath + char + extension);
+		displayAlphabet(char);
 	} else {
 		audioContainer.removeAttribute('src');
 		console.log("Key is not found!", e.keyCode);
@@ -19,8 +19,16 @@ document.onkeydown = function(e) {
 };
 
 
-function displayAlphabet (alphabet) {
+function displayAlphabet (char) {
+	let regex = /[0-9]/g;
 	let alphabetContainer = document.getElementById('alphabet-container');
 	let staticText = 'Displaying alphabet ~ ';
-	alphabetContainer.innerHTML = staticText + alphabet;
+	let staticNumber = 'Displaying number ~ ';
+	let fullText;
+	if (!char.match(regex)) {
+		fullText = staticText + char;
+	} else {
+		fullText = staticNumber + char;
+	}
+	alphabetContainer.innerHTML = fullText;
 }
